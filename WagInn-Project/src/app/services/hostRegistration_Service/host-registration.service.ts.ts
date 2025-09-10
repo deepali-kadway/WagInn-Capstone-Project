a5b142a4cbf91d9647cb423a9a9c931b98d18e7c
Deepali Kadway <dkadway@gmail.com>
2025-09-10 17:26:27 -0600

    service accepting, generating and sending payload to backend

--- WagInn-Project/src/app/services/hostRegistration_Service/host-registration.service.ts.ts
+++ WagInn-Project/src/app/services/hostRegistration_Service/host-registration.service.ts.ts
@@ -5,74 +5,99 @@ import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HostRegistrationServiceTs {
-private apiUrl = `${environment.apiUrl}/host/register`
+  private apiUrl = `${environment.apiUrl}/host/register`;
 
-private hostregistrationData: Partial<HostRegistrationData> = {}
+  private hostregistrationData: Partial<HostRegistrationData> = {};
 
-constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {}
 
-//step wise data updates
-updatePersonalInfo(data: any){
-  this.hostregistrationData.personalInfo = data;
-}
+  //step wise data updates
+  updatePersonalInfo(data: any) {
+    this.hostregistrationData.personalInfo = data;
+  }
 
-updateAddressDetails(data: any) {
+  updateAddressDetails(data: any) {
     this.hostregistrationData.addressDetails = data;
   }
-  
-updatePropertyDetails(data: any) {
+
+  updatePropertyDetails(data: any) {
     this.hostregistrationData.propertyDetails = data;
   }
-  
-updatePetInfo(data: any) {
+
+  updatePetInfo(data: any) {
     this.hostregistrationData.petInfo = data;
   }
-  
-updatePricing(data: any) {
+
+  updatePricing(data: any) {
     this.hostregistrationData.pricing = data;
   }
- 
-updateIdVerification(data: any) {
+
+  updateIdVerification(data: any) {
     this.hostregistrationData.idVerification = data;
   }
 
-//get complete data
-getRegistrationData(): Partial<HostRegistrationData> {
-  return this.hostregistrationData;
-}
-
-//submit to backend
-submitRegistration(): Observable<any> {
-  const formData = new FormData();
-
-  formData.append('personalInfo', JSON.stringify(this.hostregistrationData.personalInfo))
-  formData.append('addressDetails', JSON.stringify(this.hostregistrationData.addressDetails))
-  formData.append('propertyDetails', JSON.stringify(this.hostregistrationData.propertyDetails))
-  formData.append('petInfo', JSON.stringify(this.hostregistrationData.petInfo))
-  formData.append('pricing', JSON.stringify(this.hostregistrationData.pricing))
-  formData.append('idVerification', JSON.stringify(this.hostregistrationData.idVerification))
-
-  if(this.hostregistrationData.propertyDetails?.propertyPhotos){
-    this.hostregistrationData.propertyDetails.propertyPhotos.forEach((file, index) => {
-      formData.append(`propertyPhoto_${index}`, file)
-    })
+  //get complete data
+  getRegistrationData(): Partial<HostRegistrationData> {
+    return this.hostregistrationData;
   }
 
-  if(this.hostregistrationData.idVerification?.frontIdFile){
-    formData.append('frontId', this.hostregistrationData.idVerification.frontIdFile)
-  }
+  //submit to backend
+  submitRegistration(): Observable<any> {
+    const formData = new FormData();
 
-  
-  if(this.hostregistrationData.idVerification?.backIdFile){
-    formData.append('backId', this.hostregistrationData.idVerification.backIdFile)
-  }
+    formData.append(
+      'personalInfo',
+      JSON.stringify(this.hostregistrationData.personalInfo)
+    );
+    formData.append(
+      'addressDetails',
+      JSON.stringify(this.hostregistrationData.addressDetails)
+    );
+    formData.append(
+      'propertyDetails',
+      JSON.stringify(this.hostregistrationData.propertyDetails)
+    );
+    formData.append(
+      'petInfo',
+      JSON.stringify(this.hostregistrationData.petInfo)
+    );
+    formData.append(
+      'pricing',
+      JSON.stringify(this.hostregistrationData.pricing)
+    );
+    formData.append(
+      'idVerification',
+      JSON.stringify(this.hostregistrationData.idVerification)
+    );
 
-  return this.http.post(this.apiUrl, formData)
-}
-  clearRegistrationData(){
-    this.hostregistrationData = {}
+    if (this.hostregistrationData.propertyDetails?.propertyPhotos) {
+      this.hostregistrationData.propertyDetails.propertyPhotos.forEach(
+        (file, index) => {
+          formData.append(`propertyPhoto_${index}`, file);
+        }
+      );
+    }
+
+    if (this.hostregistrationData.idVerification?.frontIdFile) {
+      formData.append(
+        'frontId',
+        this.hostregistrationData.idVerification.frontIdFile
+      );
+    }
+
+    if (this.hostregistrationData.idVerification?.backIdFile) {
+      formData.append(
+        'backId',
+        this.hostregistrationData.idVerification.backIdFile
+      );
+    }
+
+    return this.http.post(this.apiUrl, formData);
+  }
+  clearRegistrationData() {
+    this.hostregistrationData = {};
   }
 }
