024e14a302d8dd41c745410bad4cb3eddbf4c36d
Deepali Kadway <dkadway@gmail.com>
2025-09-11 14:35:52 -0600

    database & server connection, model creation

---
+++ WagInn-Backend/models/hostRegistration_Model.js
@@ -0,0 +1,162 @@
+import sequelize from "../config.js";
+import { DataTypes } from "sequelize";
+
+const Host = sequelize.define("Host_Profile", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  //personal info
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  birthMonth: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  birthDay: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  birthYear: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: { isEmail: true },
+  },
+
+  // Address Info
+  streetAddress: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  province: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  zipCode: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  country: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  //Pet Info
+  allowedPetsType: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  petSizeRestrictions: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  numberOfPetsAllowed: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  houseRules: {
+    type: DataTypes.TEXT,
+  },
+  requiredVaccinations: {
+    type: DataTypes.STRING,
+  },
+  neuteredSpayedRequired: {
+    type: DataTypes.STRING,
+  },
+  fleaTickPreventionRequired: {
+    type: DataTypes.STRING,
+  },
+
+  //property info
+  propertyTitle: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  propertyType: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  ammenities: {
+    type: DataTypes.TEXT,
+  },
+  guests: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  bedrooms: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  beds: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  bathrooms: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  pets: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  ammenitiesPets: {
+    type: DataTypes.TEXT,
+  },
+  propertyPhotos: {
+    type: DataTypes.JSON,
+    defaultValue: [],
+  },
+
+  //Pricing
+  basePrice: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  totalGuestPrice: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  hostEarnings: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+
+  // ID Verification
+  frontIdUrl: {
+    type: DataTypes.STRING,
+  },
+  backIdUrl: {
+    type: DataTypes.STRING,
+  },
+
+  // Overall Status
+  registrationStatus: {
+    type: DataTypes.ENUM("pending", "active", "suspended", "rejected"),
+    defaultValue: "pending",
+  },
+  registrationCompletedAt: {
+    type: DataTypes.DATE,
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+});
+
+export default Host;
---
+++ WagInn-Backend/server.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import cors from 'cors'
+import sequelize from "./config.js";
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+//Test DB connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connection established successfully");
+  })
+  .catch((error) => {
+    console.log("Unable to connect to the database:", error.message);
+  });
+
+// Sync models to database and create tables if they don't exist
+console.log('About to sync models...');
+
+sequelize
+  .sync({force: false})
+  .then(() => {
+    console.log("Database tables created successfully");
+  })
+  .catch((error) => {
+    console.log("Error in creating DB tables: ", error.message);
+  });
+
+  //debug steps
+  console.log('Available models:', Object.keys(sequelize.models));
+  console.log('Connected to database:', sequelize.config.database);
+
+//Listen on Port 3004
+app.listen(3004, () => {
+  console.log("Server is running on port 3004");
+});
