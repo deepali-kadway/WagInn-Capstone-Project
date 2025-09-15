26cfaa080645b796fedfed947677dd5e8e8855c2
Deepali Kadway <dkadway@gmail.com>
2025-09-15 13:21:56 -0600

    navigation for becoming a host

--- WagInn-Project/src/app/components/header/header.ts
+++ WagInn-Project/src/app/components/header/header.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: false,
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
 })
 export class Header {
+  constructor(private router: Router) {}
 
+  navigateToRegister() {
+    this.router.navigate(['']);
+  }
 }
